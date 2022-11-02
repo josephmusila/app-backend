55090d060d8aa7f8f408360b18413df4a0e94b4a
Musila <josemusila03@gmail.com>
2022-11-02 12:17:55 +0300

    added test files

--- sahabackend/settings.py
+++ sahabackend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'super_inlines'
+    'super_inlines',
     'rest_framework',
     'django.contrib.staticfiles',
     'corsheaders',
